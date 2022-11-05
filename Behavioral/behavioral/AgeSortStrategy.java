350888cb6bd19a734c4492254ce15a1bd32b9af6
Glit <69191238+tsunafield1@users.noreply.github.com>
2022-11-05 19:14:36 +0700

    Add Behavioral Design Pattern Assignment

---
+++ Behavioral/behavioral/AgeSortStrategy.java
@@ -0,0 +1,15 @@
+package com.behavioral;
+
+import java.util.Comparator;
+import java.util.List;
+
+public class AgeSortStrategy implements SortStrategy {
+    @Override
+    public void customSort(List<Person> people) {
+        // YOU ARE NOT ALLOWED TO MODIFY THE CODE ABOVE THIS COMMENT !!
+        // YOU MAY ADD UP TO 5 LINES OF CODE BELOW THIS COMMENT !! A LINE OF CODE MAY CONTAIN UP TO ONE SEMI-COLON !!
+        Comparator cmp = Comparator.comparing(Person::getAge);
+        people.sort(cmp);
+    }
+
+}
---
+++ Behavioral/behavioral/SortStrategy.java
@@ -0,0 +1,8 @@
+package com.behavioral;
+
+// DO NOT MODIFY ANYTHING BELOW THIS LINE!!
+import java.util.List;
+
+public interface SortStrategy {
+    void customSort(List<Person> people);
+}
